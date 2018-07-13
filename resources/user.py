e027024725569a90e4493f3951d1a7427452d568
francasense <francasense@gmail.com>
2018-07-13 19:13:35 -0300

    user.py
    flag add

--- resources/user.py
+++ resources/user.py
@@ -44,12 +44,12 @@ class UserRegister(Resource):
         data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
-            return {"message": "A user with that username already exists"}, 400
+            return {"message": "A user with that username already exists", "st":"2"}, 400
 
         user = UserModel(**data)
         user.save_to_db()
 
-        return {"message": "User created successfully."}, 201
+        return {"message": "User created successfully.", "st":"1"}, 201
 
 
 class User(Resource):
