2b885c9890db5178ce277747bec48b1f4020098d
francasense <francasense@gmail.com>
2018-07-19 18:43:20 -0300

    Create endereco.py

---
+++ models/endereco.py
@@ -0,0 +1,53 @@
+from db import db
+
+
+class EderecoModel(db.Model):
+    __tablename__ = 'enderecos'
+
+    cep = db.Column(db.Integer, primary_key=True)
+    rua = db.Column(db.String(80))
+    bairro = db.Column(db.String(40))
+    cidade = db.Column(db.String(80))
+    estado = db.Column(db.String(40))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('UserModel')
+
+    def __init__(self, cep, rua, bairro, cidade, estado,  user_id):
+        self.name = cep
+        self.idade = rua
+        self.name = bairro
+        self.idade = cidade
+        self.name = estado
+        self.user_id = user_id
+
+    def json(self):
+        return {
+            'id': self.id,
+            'name': self.cep,
+            'idade': self.rua,
+            'name': self.bairro,
+            'idade': self.cidade,
+            'name': self.estado,
+            'user_id': self.user_id
+        }
+
+    @classmethod
+    def find_by_name(cls, cep):
+        return cls.query.filter_by(cep=cep).first()
+
+    @classmethod
+    def find_by_id(cls, _id):
+        return cls.query.filter_by(user_id=_id).all()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
