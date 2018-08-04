fa9353ff6615ff3ff8a5e2d0db430174a6c8bbd2
francasense <francasense@gmail.com>
2018-08-03 23:22:05 -0300

    Update user.py

--- resources/user.py
+++ resources/user.py
@@ -2,7 +2,7 @@ from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
 from models.user import UserModel
-from flask import session
+
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument('username',
@@ -80,7 +80,6 @@ class User(Resource):
 
 
 class UserLogin(Resource):
-    session.permanent = True
     def post(self):
         data = _user_parser.parse_args()
 
