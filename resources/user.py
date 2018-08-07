a4624a3604c27da7e2fdee511111ad86451c03f9
francasense <francasense@gmail.com>
2018-08-07 20:48:44 -0300

    Update user.py

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
+from flask_jwt_extended import create_access_token,  jwt_required, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
 from models.user import UserModel
 
 
