653dc1181eb55fb082ad5f70b3a92709a0245e7d
francasense <francasense@gmail.com>
2018-08-15 02:03:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -11,10 +12,10 @@ from resources.endereco import Endereco, EnderecoList, EnderecoSelecao, Endereco
 from resources.teste import Teste
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'  # could do app.config['JWT_SECRET_KEY'] if we prefer
+app.secret_key = 'egfrdfsre'  # could do app.config['JWT_SECRET_KEY'] if we prefer
 api = Api(app)
 
 
