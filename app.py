b86df712f70a7a92dcc7bf62d96bf74d840bd3bd
francasense <francasense@gmail.com>
2018-09-06 19:14:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
+from db import db
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from db import db
+
 
 from resources.osseca import TempLogin
+from resources.usertemp import UserRegisterTemp, UserTemp, Operacao
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.dependente import Dependente, DependenteList, DependenteSelecao, DependenteDelete
 from resources.controle import Controle, ControleList, ControleSelecao
@@ -21,8 +23,8 @@ POSTGRES = {
     'host': 'ec2-54-83-13-119.compute-1.amazonaws.com',
     'port': '5432',
 }
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ekgznjyknrrepu:efd0b2e280437c4dd5ba5d7aaa0cff2f40221dae42eae81cbc460c67b4fbc3bf@ec2-54-83-13-119.compute-1.amazonaws.com:5432/dcp3rbfue209db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('URI','sqlite:///data.db')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ekgznjyknrrepu:efd0b2e280437c4dd5ba5d7aaa0cff2f40221dae42eae81cbc460c67b4fbc3bf@ec2-54-83-13-119.compute-1.amazonaws.com:5432/dcp3rbfue209db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('URI','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'egfrdfsre'  # could do app.config['JWT_SECRET_KEY'] if we prefer
@@ -44,9 +46,9 @@ def add_claims_to_jwt(identity):  # Remember identity is what we define when cre
     return {'is_admin': False}
 """
 
-#@app.before_first_request
-#def create_tables():
-#    db.create_all()
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 #@jwt.expired_token_loader
 #def expired_token_callback():
@@ -76,7 +78,11 @@ api.add_resource(EnderecoList, '/enderecos')
 api.add_resource(EnderecoSelecao, '/enderecoselecao/')#EnderecoDelete
 api.add_resource(EnderecoDelete, '/enderecodelete/<int:id>')
 
-api.add_resource(UserRegister, '/register')
+api.add_resource(UserRegisterTemp, '/registertemp')
+api.add_resource(Operacao, '/operacao/<int:id>')
+api.add_resource(UserTemp, '/usertemp/<int:id>')
+
+#api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:id>')
 api.add_resource(UserLogin, '/login')
 api.add_resource(Teste, '/teste')
