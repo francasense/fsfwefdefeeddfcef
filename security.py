e53ae243c4434503d3dba160b0d1c467fb39bd43
francasense <francasense@gmail.com>
2018-09-15 16:23:44 -0300

    Update security.py

--- security.py
+++ security.py
@@ -6,7 +6,11 @@ def authenticate(username, password):
     user = UserModel.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
-
+    
+def authenticate(username, password):
+    gestor = GestorModel.find_by_username(username)
+    if gestor and safe_str_cmp(gestor.password, password):
+        return gestor    
 
 def identity(payload):
     user_id = payload['identity']
