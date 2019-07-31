67bf38b9f23b9a3158c11051e10b430589459dee
francasense <francasense@gmail.com>
2019-07-31 14:11:43 -0300

    Update igreja.py

--- models/igreja.py
+++ models/igreja.py
@@ -5,23 +5,25 @@ class IgrejaModel(db.Model):
     __tablename__ = 'igreja'
 
     id = db.Column(db.Integer, primary_key=True)
+    tipo = db.Column(db.String(50))
     cnpj = db.Column(db.String(100))
     razao_social = db.Column(db.String(100))
     natureza_juridica = db.Column(db.String(100))
     data_fundacao = db.Column(db.String(100))
-    responsavel = db.Column(db.String(150))
-    logradouro = db.Column(db.String(20))
+    responsavel = db.Column(db.String(100))
+    logradouro = db.Column(db.String(100))
     numero = db.Column(db.String(10))
-    bairro = db.Column(db.String(10))
+    bairro = db.Column(db.String(100))
     complemento = db.Column(db.String(30))
     cep = db.Column(db.String(30))
-    cidade = db.Column(db.String(30))
+    cidade = db.Column(db.String(150))
     telefone = db.Column(db.String(30))
     uf = db.Column(db.String(30))
 
-    def __init__(cnpj, razao_social, natureza_juridica, data_fundacao,responsavel,logradouro,numero,bairro,complemento,cep,cidade,telefone,uf):
+    def __init__(tipo, cnpj, razao_social, natureza_juridica, data_fundacao,responsavel,logradouro,numero,bairro,complemento,cep,cidade,telefone,uf):
 
         self.cnpj = cnpj
+        self.tipo = tipo
         self.razao_social = razao_social
         self.natureza_juridica = natureza_juridica
         self.data_fundacao = data_fundacao
@@ -38,6 +40,7 @@ class IgrejaModel(db.Model):
     def json(self):
         return {
             'id': self.id,
+            'tipo': self.tipo,
             'cnpj': self.cnpj,
             'razao_social': self.razao_social,
             'natureza_juridica': self.natureza_juridica,
@@ -70,6 +73,11 @@ class IgrejaModel(db.Model):
     def find_by_id_unique(cls, id):
         return cls.query.filter_by(id=id).first()
 
+   
+    @classmethod
+    def find_by_tipo(cls, tipo):
+        return cls.query.filter_by(tipo=tipo).all()
+    
     @classmethod
     def find_all(cls):
         return cls.query.all()
