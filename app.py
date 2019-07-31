9488605d9d24a31f4ca3222aa04041d46c1cd9c0
francasense <francasense@gmail.com>
2019-07-31 14:57:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 
-from resources.osseca import TempLogin
 from resources.igreja import Igreja, Igrejaunica, Igrejatipo
-
-from resources.teste import Teste
 
 app = Flask(__name__)
 
