441777b10f8901c7acb0e02a2cfc6fa8b59d1441
francasense <francasense@gmail.com>
2019-07-31 16:25:07 -0300

    Update igreja.py

--- models/igreja.py
+++ models/igreja.py
@@ -5,7 +5,6 @@ class IgrejaModel(db.Model):
     __tablename__ = 'igreja'
 
     id = db.Column(db.Integer, primary_key=True)
-    evento = db.Column(db.String(3))
     tipo = db.Column(db.String(50))
     cnpj = db.Column(db.String(100))
     razao_social = db.Column(db.String(100))
@@ -22,39 +21,13 @@ class IgrejaModel(db.Model):
     uf = db.Column(db.String(30))
     latitude = db.Column(db.String(30))
     longitude = db.Column(db.String(30))
-    ru_reuniao = db.Column(db.String(100))
-    ru_data_inicial = db.Column(db.String(30))
-    ru_data_final = db.Column(db.String(30))
-    ru_horario_entrada = db.Column(db.String(30))
-    ru_horario_saida = db.Column(db.String(30))
-    ru_responsavel = db.Column(db.String(100))
-    ru_aberto_publico = db.Column(db.String(3))
-    ru_telefone = db.Column(db.String(30))
-    ru_titulo = db.Column(db.String(30))
-    ru_descricao = db.Column(db.String(300))
-    ru_local = db.Column(db.String(100))
-    ev_nome = db.Column(db.String(100))
-    ev_data_inicial = db.Column(db.String(30))
-    ev_data_final = db.Column(db.String(30))
-    ev_horario_entrada = db.Column(db.String(30))
-    ev_horario_saida = db.Column(db.String(30))
-    ev_responsavel = db.Column(db.String(100))
-    ev_aberto_publico = db.Column(db.String(3))
-    ev_telefone = db.Column(db.String(30))
-    ev_valor = db.Column(db.String(30))
-    ev_local = db.Column(db.String(100))
     img_igreja = db.Column(db.String(200))
-    img_ru = db.Column(db.String(200))
-    img_ev = db.Column(db.String(200))
 
 
-    def __init__(img_igreja,img_ru,img_ev,evento,ru_reuniao,ru_data_inicial,ru_data_final,ru_horario_entrada,ru_horario_saida,ru_responsavel,ru_aberto_publico,ru_telefone,ru_titulo,ru_descricao,ru_local,ev_nome,ev_data_inicial,ev_data_final,ev_horario_entrada,ev_horario_saida,ev_responsavel,ev_aberto_publico,ev_telefone,ev_valor,ev_local,latitude, longitude, tipo, cnpj, razao_social, natureza_juridica, data_fundacao,responsavel,logradouro,numero,bairro,complemento,cep,cidade,telefone,uf):
+    def __init__(img_igreja,latitude, longitude, tipo, cnpj, razao_social, natureza_juridica, data_fundacao,responsavel,logradouro,numero,bairro,complemento,cep,cidade,telefone,uf):
 
         self.img_igreja = img_igreja
-        self.img_ru = img_ru
-        self.img_ev = img_ev
         self.cnpj = cnpj
-        self.evento = evento
         self.tipo = tipo
         self.razao_social = razao_social
         self.natureza_juridica = natureza_juridica
@@ -70,35 +43,12 @@ class IgrejaModel(db.Model):
         self.uf = uf
         self.latitude = latitude
         self.longitude = longitude
-        self.ru_reuniao = ru_reuniao
-        self.ru_data_inicial = ru_data_inicial
-        self.ru_data_final = ru_data_final
-        self.ru_horario_entrada = ru_horario_entrada
-        self.ru_horario_saida = ru_horario_saida
-        self.ru_responsavel = ru_responsavel
-        self.ru_aberto_publico = ru_aberto_publico
-        self.ru_telefone = ru_telefone
-        self.ru_titulo = ru_titulo
-        self.ru_descricao = ru_descricao
-        self.ru_local = ru_local
-        self.ev_nome = ev_nome
-        self.ev_data_inicial = ev_data_inicial
-        self.ev_data_final = ev_data_final
-        self.ev_horario_entrada = ev_horario_entrada
-        self.ev_horario_saida = ev_horario_saida
-        self.ev_responsavel = ev_responsavel
-        self.ev_aberto_publico = ev_aberto_publico
-        self.ev_telefone = ev_telefone
-        self.ev_valor = ev_valor
-        self.ev_local = ev_local
+
 
     def json(self):
         return {
             'id': self.id,
             'img_igreja': self.img_igreja,
-            'img_ru': self.img_ru,
-            'img_ev': self.img_ev,
-            'evento': self.evento,
             'tipo': self.tipo,
             'cnpj': self.cnpj,
             'razao_social': self.razao_social,
@@ -112,28 +62,7 @@ class IgrejaModel(db.Model):
             'cep': self.cep,
             'cidade': self.cidade,
             'telefone': self.telefone,
-            'uf': self.uf,
-            'ru_reuniao': self.ru_reuniao,
-            'ru_data_inicial': self.ru_data_inicial, 
-            'ru_data_final': self.ru_data_final, 
-            'ru_horario_entrada': self.ru_horario_entrada, 
-            'ru_horario_saida': self.ru_horario_saida,           
-            'ru_responsavel': self.ru_responsavel, 
-            'ru_aberto_publico': self.ru_aberto_publico, 
-            'ru_telefone': self.ru_telefone, 
-            'ru_titulo': self.ru_titulo, 
-            'ru_descricao': self.ru_descricao, 
-            'ru_local': self.ru_local, 
-            'ev_nome': self.ev_nome, 
-            'ev_data_inicial': self.ev_data_inicial, 
-            'ev_data_final': self.ev_data_final, 
-            'ev_horario_entrada': self.ev_horario_entrada, 
-            'ev_horario_saida': self.ev_horario_saida, 
-            'ev_responsavel': self.ev_responsavel, 
-            'ev_aberto_publico': self.ev_aberto_publico, 
-            'ev_telefone': self.ev_telefone, 
-            'ev_valor': self.ev_valor, 
-            'ev_local': self.ev_local, 
+            'uf': self.uf, 
             'latitude': self.latitude, 
             'longitude': self.longitude, 
             
