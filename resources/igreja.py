9b843e6759b81180c51c8bc29e164b84c652e0a2
francasense <francasense@gmail.com>
2019-07-31 16:22:42 -0300

    Update igreja.py

--- resources/igreja.py
+++ resources/igreja.py
@@ -9,16 +9,6 @@ class Igreja(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
-    parser.add_argument('img_ru',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('img_ev',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
     parser.add_argument('tipo',
                         type=str,
                         required=True,
@@ -89,117 +79,7 @@ class Igreja(Resource):
                         required=True,
                         help="Every estabelecimento needs a valor_contrato."
                         )
-    parser.add_argument('evento',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
     parser.add_argument('tipo',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_reuniao',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_data_inicial',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_data_final',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_horario_entrada',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_horario_saida',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_responsavel',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_aberto_publico',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_telefone',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_titulo',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_descricao',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ru_local',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_nome',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_data_inicial',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_data_final',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_horario_entrada',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_horario_saida',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_responsavel',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_aberto_publico',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_telefone',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_valor',
-                        type=str,
-                        required=True,
-                        help="Every estabelecimento needs a valor_contrato."
-                        )
-    parser.add_argument('ev_local',
                         type=str,
                         required=True,
                         help="Every estabelecimento needs a valor_contrato."
@@ -216,14 +96,12 @@ class Igreja(Resource):
                         )
 
 
-    @jwt_required  # No longer needs brackets
     def get(self, nome):
         igreja = IgrejaModel.find_by_name(razao_social)
         if igreja:
             return (igreja.json(), {'message': 'Dados do estabelecimento', 'st':'1'}), 201
         return {'message': 'igreja not found'}, 404
 
-    @jwt_required
     def post(self):
 
         data = Igreja.parser.parse_args()
